189216f139d0e28d6da7465768677063f5c6aef7
Mjohn572 <mjohn572@mtroyal.ca>
2026-04-15 15:32:15 -0600

    Changed Config to work with render

--- A2-react-app/vite.config.js
+++ A2-react-app/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/COMP4513-Assignment-2/',
+  base: '/',
   plugins: [react(),
     tailwindcss()
   ],
